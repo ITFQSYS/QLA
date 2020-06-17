e0dc729a3c5a18e65b2b8abecfa1838ce1fa91e3
Q M <qdevelop@hotmail.com>
2020-06-17 23:59:15 +0900

    add: LU分解と連立方程式の直接法ができた!

--- include/Linalg.hpp
+++ include/Linalg.hpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <iostream>
 #include <limits>
+#include <omp.h>
 
 namespace QLA
 {
@@ -64,6 +65,146 @@ namespace QLA
         }
         return true;
     }
+
+    //LU分解を行う ピボット探索の結果は置換行列Pに出力される
+    //PA=LU の関係がある.
+    // 参考: http://www.ritsumei.ac.jp/se/~hirai/edu/2019/algorithm/handout/linear_8up.pdf
+    void lu(Matrix a, Matrix &l, Matrix &u, Matrix &p)
+    {
+        assert(a.getRows() == a.getCols()); //Aが正方行列
+        int n = a.getCols();                //行列のサイズ
+
+        //Lのサイズが正しいか?
+        assert(l.getRows() == l.getCols());
+        assert(l.getRows() == n);
+        //Uのサイズが正しいか?
+        assert(u.getRows() == u.getCols());
+        assert(u.getRows() == n);
+        //Pのサイズが正しいか?
+        assert(p.getRows() == p.getCols());
+        assert(p.getRows() == n);
+
+        std::vector<int> index(n); //置換の記録
+
+        //変数の初期化
+        //#pragma omp parallel for
+        for (int i = 0; i < n * n; i++)
+        {
+            l(i) = u(i) = p(i) = 0;
+        }
+        //#pragma omp parallel for
+        for (int i = 0; i < n; i++)
+        {
+            l(i, i) = 1;
+            index[i] = i;
+        }
+
+        for (int k = 0; k < n; k++)
+        {
+            int p = k;
+            double a_max = abs(a(k, k));
+
+            //ピボットを探す
+            for (int j = k + 1; j < n; j++)
+            {
+                if (a_max < abs(a(k, j)))
+                {
+                    p = j;
+                    a_max = abs(a(k, j));
+                }
+            }
+
+            //Aの行の入れ替え
+            for (int j = k; j < n; j++)
+            {
+                std::swap(a(k, j), a(p, j));
+            }
+            //Lの行の入れ替え
+            for (int j = 0; j < k; j++)
+            {
+                std::swap(l(k, j), l(p, j));
+            }
+
+            std::swap(index[k], index[p]); //置換の記録
+
+            for (int j = k; j < n; j++)
+            {
+                u(k, j) = a(k, j);
+            }
+
+            for (int i = k + 1; i < n; i++)
+            {
+                l(i, k) = a(i, k) / u(k, k);
+            }
+
+            for (int i = k + 1; i < n; i++)
+            {
+                for (int j = k + 1; j < n; j++)
+                {
+                    a(i, j) -= l(i, k) * u(k, j);
+                }
+            }
+        }
+        //Pの復元
+        for (int k = 0; k < n; k++)
+        {
+            p(k, index[k]) = 1;
+        }
+    }
+
+    //連立方程式Ax=bを直接解く
+    bool solve(const Matrix &a, Matrix &x, const Matrix &b)
+    {
+        //連立方程式の条件チェック
+        assert(a.getCols() == a.getRows());
+        assert(a.getCols() == b.getRows());
+        assert(b.getCols() == 1);
+
+        int n = a.getCols(); //行列のサイズ
+
+        if (x.getCols() != 1 || x.getRows() != n) //xのサイズ調整
+        {
+            x = Matrix(n, 1);
+        }
+
+        Matrix l(n, n), u(n, n), p(n, n);
+
+        lu(a, l, u, p); //LU分解
+
+        Matrix pb = p.dot(b);
+
+        //Ax=bの両辺にpを掛けてpA=Pbを解く.
+        //pA=LUだから LU=pbを解く
+
+        Matrix y(n, 1, 0);
+
+        //Ly=_bを解く
+        for (int i = 0; i < n; i++)
+        {
+            double sum = 0;
+#pragma omp parallel for
+            for (int j = 0; j <= i - 1; j++)
+            {
+                sum += l(i, j) * y(j);
+            }
+            y(i) = pb(i) - sum;
+        }
+
+        //Ux=yを解く
+        for (int i = n - 1; i >= 0; --i)
+        {
+            double sum = 0;
+
+#pragma omp parallel for
+            for (int j = i + 1; j < n; j++)
+            {
+                sum += u(i, j) * x(j);
+            }
+            x(i) = (y(i) - sum) / u(i, i);
+        }
+        return true;
+    }
+
 } // namespace QLA
 
 #endif
--- test/test_linalg.hpp
+++ test/test_linalg.hpp
@@ -3,7 +3,7 @@
 using namespace QLA;
 using namespace std;
 
-TEST(LinalgTest,SOR_test)
+TEST(LinalgTest, SOR_test)
 {
     Matrix
         a = {10., 3., 1., 2., 1.,
@@ -20,5 +20,36 @@ TEST(LinalgTest,SOR_test)
 
     solve_sor(a, x, b, 1.1, 1e-12);
 
-   EXPECT_NEAR((b-a.dot(x)).l2norm(),0,1e-5);
+    EXPECT_NEAR((b - a.dot(x)).l2norm(), 0, 1e-5);
+}
+
+TEST(LinalgTest, LU_decomposition_test)
+{
+    Matrix a = {0, 10, 20, 2, 1, 10, 10, 0, 5};
+    a.reshape(3, 3);
+
+    Matrix l(3,3), u(3,3), p(3,3);
+    lu(a, l, u, p);
+
+    EXPECT_NEAR((p.dot(a) -l.dot(u)).l2norm(),0,1e-8);
+}
+
+TEST(LinalgTest, solve_test)
+{
+    Matrix
+        a = {10., 3., 1., 2., 1.,
+             1., 19., 2., -1., 5.,
+             -1., 1., 30., 1., 10.,
+             -2., 0., 1., 20., 5.,
+             -3., 5., 1., -2., 25.};
+    a.reshape(5, 5);
+    //cout << a << endl;
+
+    Matrix b = {-22, 27, 89, -73, 22};
+    b.reshape(5, 1);
+    Matrix x(5, 1);
+
+    solve(a,x,b);
+    
+    EXPECT_NEAR((b - a.dot(x)).l2norm(), 0, 1e-8);
 }
